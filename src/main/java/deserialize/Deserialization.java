8a9fb91709da2edb497d014eb2d113486ff16cac
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-14 19:11:35 +0530

    stage 1

--- src/main/java/deserialize/Deserialization.java
+++ src/main/java/deserialize/Deserialization.java
@@ -4,14 +4,15 @@ import java.io.*;
 public class Deserialization implements Serializable {
     static String loc = "C:\\Users\\Dhinesh Kannan\\IdeaProjects\\project2\\src\\main\\resources\\e.txt";
     public Deserialization() {
+
     }
     public void deserialize() throws IOException, ClassNotFoundException {
 
             ObjectInputStream in = new ObjectInputStream(new FileInputStream(loc));
             Data der = (Data) in.readObject();
-        if(der.age>=25){
+//        if(der.age>=25){
             System.out.println(der.age+":"+der.id+":"+ der.salary+":"+der.address+":"+der.name);
-        }
+//        }
             System.out.println("deserilaize done");
             in.close();
     }
