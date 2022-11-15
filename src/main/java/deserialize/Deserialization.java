a60d621eb653366e1e93fb5db7eb958c8642214d
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-15 19:04:38 +0530

    stage 1

--- src/main/java/deserialize/Deserialization.java
+++ src/main/java/deserialize/Deserialization.java
@@ -4,21 +4,25 @@ import Serial.Data;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Deserialization implements Serializable {
-    static String loc = "C:\\Users\\Dhinesh Kannan\\IdeaProjects\\project2\\src\\main\\resources\\e0.txt";
+    static String loc = "C:\\Users\\Dhinesh Kannan\\IdeaProjects\\project2\\src\\main\\resources\\e.txt";
 
     public Deserialization() {
-
     }
+
     public void deserialize() throws IOException, ClassNotFoundException {
 
         ObjectInputStream in = new ObjectInputStream(new FileInputStream(loc));
         ArrayList<Data> list1 = (ArrayList<Data>) in.readObject();
-        for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i).age >= 25)
-                System.out.println(list1.get(i).name + ":" + list1.get(i).age + ":" + list1.get(i).id + ":" + list1.get(i).address + ":" + list1.get(i).salary);
-        }
+        list1.forEach(
+                (n) -> {
+                    if (n.age >= 25) {
+                        System.out.println(n.name + ":" + n.salary + ":" + n.id + ":" + n.age + ":" + n.address);
+                    }
+                }
+        );
         System.out.println("deserilaize done");
         in.close();
     }
