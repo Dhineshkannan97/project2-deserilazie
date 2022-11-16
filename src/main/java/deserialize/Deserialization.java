25784ec244b9195ba131c7716dab5be17768d8fb
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-16 16:12:34 +0530

    stage 1

--- src/main/java/deserialize/Deserialization.java
+++ src/main/java/deserialize/Deserialization.java
@@ -3,11 +3,12 @@ package deserialize;
 import Serial.Data;
 
 import java.io.*;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collection;
 
 public class Deserialization implements Serializable {
     static String loc = "C:\\Users\\Dhinesh Kannan\\IdeaProjects\\project2\\src\\main\\resources\\e.txt";
+    Connection conn;
 
     public Deserialization() {
     }
@@ -20,14 +21,26 @@ public class Deserialization implements Serializable {
                 (n) -> {
                     if (n.age >= 25) {
                         System.out.println(n.name + ":" + n.salary + ":" + n.id + ":" + n.age + ":" + n.address);
+                        try {
+                            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "123456");
+                            Statement stmt = conn.createStatement();
+                            ResultSet rs = stmt.executeQuery("SELECT * FROM yahoo;");
+                            String sql1 = "INSERT INTO yahoo (ID,NAME,AGE,ADDRESS,SALARY) "
+                                    + "VALUES (" + n.id + ", '" + n.name + "'," + n.age + ", '" + n.address + "'," + n.salary + " );";
+                            stmt.executeUpdate(sql1);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
+
                 }
+
         );
         System.out.println("deserilaize done");
         in.close();
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         Deserialization dsr = new Deserialization();
         dsr.deserialize();
     }
