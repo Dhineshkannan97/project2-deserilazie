d59d725ca93285133a36371d1cee82128a797c82
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-22 13:42:53 +0530

    stage 1

--- src/main/java/deserialize/Deserialization.java
+++ src/main/java/deserialize/Deserialization.java
@@ -1,7 +1,5 @@
 package deserialize;
-
 import Serial.Data;
-
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
@@ -14,7 +12,6 @@ public class Deserialization implements Serializable {
     }
 
     public void deserialize() throws IOException, ClassNotFoundException {
-
         ObjectInputStream in = new ObjectInputStream(new FileInputStream(loc));
         ArrayList<Data> list1 = (ArrayList<Data>) in.readObject();
         list1.forEach(
@@ -25,14 +22,19 @@ public class Deserialization implements Serializable {
                             conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "123456");
                             Statement stmt = conn.createStatement();
                             ResultSet rs = stmt.executeQuery("SELECT * FROM yahoo;");
+                            //Select count(*) wher id = Xx
+                            //if count == 0 then insert else update
                             String sql1 = "INSERT INTO yahoo (ID,NAME,AGE,ADDRESS,SALARY) "
                                     + "VALUES (" + n.id + ", '" + n.name + "'," + n.age + ", '" + n.address + "'," + n.salary + " );";
                             stmt.executeUpdate(sql1);
+//                            String sql2= "update yahoo\n"+
+//                                    "set ID = ID- 1\n"+
+//                                    "where ID > 6;";
+//                            stmt.executeUpdate(sql2);
                         } catch (SQLException e) {
                             throw new RuntimeException(e);
                         }
                     }
-
                 }
 
         );
